c427ed2351c76d72fc646d9f646dda33f1fd6536
B-Palavikas-CTI <b.palavikas@latrobe.edu.au>
2024-12-14 11:42:00 +1100

    Neovim added

---
+++ .config/nvim/init.lua
@@ -0,0 +1,8 @@
+-- Set up core section
+require("core.globals")
+require("core.options")
+require("core.keymaps")
+-- Packet manager
+require("config.lazy")
+-- Set the colour theme
+vim.cmd.colorscheme('gruvbox-material')
---
+++ .config/nvim/lua/plugins/comment.lua
@@ -0,0 +1,8 @@
+return{
+    "numToStr/Comment.nvim",
+    enabled = true,
+        opts = {
+        -- add options
+    },
+    lazy = false,
+}
