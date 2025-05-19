0de5af0b0dc3f5d9f8891412666d904ac8fe7654
bpalavikas <bpalavikas@protonmail.com>
2025-05-19 22:05:52 +1000

    fix(nvim): noice only cmd is pop up

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,50 +1,36 @@
 return {
- "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.debug = vim.uv.cwd():find("noice%.nvim")
-      opts.debug = false
-      opts.routes = opts.routes or {}
-      table.insert(opts.routes, {
-        filter = {
-          event = "notify",
-          find = "No information available",
-        },
-        opts = { skip = true },
-      })
-      local focused = true
-      vim.api.nvim_create_autocmd("FocusGained", {
-        callback = function()
-          focused = true
-        end,
-      })
-      vim.api.nvim_create_autocmd("FocusLost", {
-        callback = function()
-          focused = false
-        end,
-      })
-
-      table.insert(opts.routes, 1, {
-        filter = {
-          ["not"] = {
-            event = "lsp",
-            kind = "progress",
-          },
-          cond = function()
-            return not focused and false
-          end,
-        },
-        view = "notify_send",
-        opts = { stop = false, replace = true },
-      })
-
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        callback = function(event)
-          vim.schedule(function()
-            require("noice.text.markdown").keys(event.buf)
-          end)
-        end,
-      })
-      return opts
-    end,
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    require("noice").setup({
+      cmdline = {
+        enabled = true,
+        view = "cmdline_popup",
+      },
+      messages = {
+        enabled = false,
+      },
+      popupmenu = {
+        enabled = false,
+      },
+      notify = {
+        enabled = false,
+      },
+      lsp = {
+        progress = { enabled = false },
+        signature = { enabled = false },
+        hover = { enabled = false },
+        message = { enabled = false },
+      },
+      presets = {
+        command_palette = false,
+        long_message_to_split = false,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    })
+  end,
 }
