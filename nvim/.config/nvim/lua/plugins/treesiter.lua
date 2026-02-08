2ca9706d1aafc01d3e5dec642bdacabaca913792
bpalavikas <bvrfirmware@proton.me>
2026-02-08 23:34:24 +1100

    fix: nvim treesitter config file path corrected

--- nvim/.config/nvim/lua/plugins/treesiter.lua
+++ nvim/.config/nvim/lua/plugins/treesiter.lua
@@ -1,14 +1,14 @@
-return{
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-            require'nvim-treesitter.configs'.setup {
-                ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "typescript", "python", "html" },
-                sync_install = false,
-                highlight = { enable = true},
-                indent = { enable = true },
-        }
-        end,
-    }
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require("nvim-treesitter.config").setup({
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "typescript", "python", "html" },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
 }
