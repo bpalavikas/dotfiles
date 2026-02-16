248c1e2375d3f5c4c83689b72997c920d027efaa
B-Palavikas-CTI <b.palavikas@latrobe.edu.au>
2026-02-16 15:59:25 +1100

    fix: regolith work laptop X resources

--- nvim/.config/nvim/lua/plugins/treesiter.lua
+++ nvim/.config/nvim/lua/plugins/treesiter.lua
@@ -4,7 +4,7 @@ return {
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.config").setup({
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "typescript", "python", "html" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "typescript", "python", "html", "kotlin" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
